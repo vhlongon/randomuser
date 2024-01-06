f9818db7fe0137a0a3932d1d5ac8dfdadd30650c
vhlongon <vhlongon@gmail.com>
2024-01-06 20:44:34 +0100

    setup testing-library

---
+++ test.setup.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import 'vitest-dom/extend-expect';
+
+global.React = React;
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test.setup.ts'],
   },
 });
