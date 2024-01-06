03142b62102ddf55dd37bc22c47e6718de4581f9
vhlongon <vhlongon@gmail.com>
2024-01-06 23:26:29 +0100

    setup test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { defaultExclude, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./test/setup.ts'],
+    coverage: {
+      provider: 'istanbul',
+      exclude: [
+        ...defaultExclude,
+        '.next/**',
+        'next.config.js',
+        'postcss.config.js',
+        'tailwind.config.ts',
+        'test/**',
+      ],
+    },
   },
   resolve: {
     alias: {
