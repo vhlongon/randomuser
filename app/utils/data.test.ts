d06a7f816ecbf187468f87e5f4552acf4b236d4f
vhlongon <vhlongon@gmail.com>
2024-01-06 20:28:40 +0100

    add util to fetch and validate data

---
+++ app/utils/data.test.ts
@@ -0,0 +1,99 @@
+import { validateData } from './data';
+
+describe('data utils', () => {
+  describe('validateData', () => {
+    it('should throw and data is invalid', () => {
+      expect(() => validateData('invalid')).toThrow();
+    });
+
+    it('should throw and with correct message where data is not valid', () => {
+      const data = {
+        results: [
+          {
+            name: {
+              title: 'Mr',
+              last: 'Gibson',
+            },
+          },
+        ],
+      };
+      expect(() => validateData(data)).toThrowError(
+        /Required in results\.0\.name\.first/
+      );
+    });
+
+    it('should return user and info when data is valid', () => {
+      const data = {
+        results: [
+          {
+            gender: 'female',
+            name: {
+              title: 'Mr',
+              first: 'Gibson',
+              last: 'Banks',
+            },
+            location: {
+              street: {
+                number: 123,
+                name: 'Mockingbird Hill',
+              },
+              city: 'San Antonio',
+              state: 'Texas',
+              country: 'United States',
+              postcode: '12345',
+              coordinates: {
+                latitude: '123',
+                longitude: '123',
+              },
+              timezone: {
+                offset: '-1:00',
+                description: 'Azores, Cape Verde Islands',
+              },
+            },
+            email: 'email@email.com',
+            login: {
+              uuid: '123',
+              username: 'username',
+              password: 'password',
+              salt: 'salt',
+              md5: 'md5',
+              sha1: 'sha1',
+              sha256: 'sha256',
+            },
+            dob: {
+              date: '2020-01-01T00:00:00.000Z',
+              age: 123,
+            },
+            registered: {
+              date: '2020-01-01T00:00:00.000Z',
+              age: 123,
+            },
+            phone: '123',
+            cell: '123',
+            id: {
+              name: 'name',
+              value: 'value',
+            },
+            picture: {
+              large: 'http://large.com',
+              medium: 'http://medium.com',
+              thumbnail: 'http://thumbnail.com',
+            },
+            nat: 'nat',
+          },
+        ],
+        info: {
+          seed: 'seed',
+          results: 123,
+          page: 123,
+          version: 'version',
+        },
+      };
+
+      expect(validateData(data)).toEqual({
+        user: data.results[0],
+        info: data.info,
+      });
+    });
+  });
+});
---
+++ app/utils/data.ts
@@ -0,0 +1,102 @@
+import { z } from 'zod';
+
+export const userSchema = z.object({
+  gender: z.union([z.literal('female'), z.literal('male')]),
+  name: z.object({
+    title: z.string(),
+    first: z.string(),
+    last: z.string(),
+  }),
+  location: z.object({
+    street: z.object({
+      number: z.number(),
+      name: z.string(),
+    }),
+    city: z.string(),
+    state: z.string(),
+    country: z.string(),
+    postcode: z.string().or(z.number()).transform(String),
+    coordinates: z.object({
+      latitude: z.string(),
+      longitude: z.string(),
+    }),
+    timezone: z.object({
+      offset: z.string(),
+      description: z.string(),
+    }),
+  }),
+  email: z.string().email(),
+  login: z.object({
+    uuid: z.string(),
+    username: z.string(),
+    password: z.string(),
+    salt: z.string(),
+    md5: z.string(),
+    sha1: z.string(),
+    sha256: z.string(),
+  }),
+  dob: z.object({
+    date: z.string().datetime(),
+    age: z.number().positive(),
+  }),
+  registered: z.object({
+    date: z.string().datetime(),
+    age: z.number().positive(),
+  }),
+  phone: z.string(),
+  cell: z.string(),
+  id: z.object({
+    name: z.string(),
+    value: z.string().nullable(),
+  }),
+  picture: z.object({
+    large: z.string().url(),
+    medium: z.string().url(),
+    thumbnail: z.string().url(),
+  }),
+  nat: z.string(),
+});
+
+export const infoSchema = z.object({
+  seed: z.string(),
+  results: z.number().positive(),
+  page: z.number().positive(),
+  version: z.string(),
+});
+
+const dataSchema = z.object({
+  results: z.array(userSchema),
+  info: infoSchema,
+});
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+export const validateData = (data: unknown) => {
+  const validatedData = dataSchema.safeParse(data);
+
+  if (!validatedData.success) {
+    const errors = validatedData.error.issues
+      .map(issue => `${issue.message} in ${issue.path.join('.')}`)
+      .join('\n');
+    throw new Error(errors);
+  }
+
+  const { results, info } = validatedData.data;
+
+  return { user: results[0], info };
+};
+
+export const fetchData = async () => {
+  // simulate more network latency
+  await sleep(0);
+  const res = await fetch('https://randomuser.me/api/', {
+    cache: 'no-cache',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  return validateData(data);
+};
