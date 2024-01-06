cdd12bc7d9f27b9db8978908bd1642fcf9ff49a6
vhlongon <vhlongon@gmail.com>
2024-01-06 20:45:42 +0100

    add random user to show on main page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { UserCard } from './components/UserCard';
+import { fetchData } from './utils/data';
 
-export default function Home() {
+export default async function Home() {
+  const { user, info } = await fetchData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main
+    <main className="flex min-h-screen flex-col justify-center items-center p-24 gap-8">
+      <UserCard user={user} info={info} />
     </main>
   );
 }
