7df1bd1817942e090d9f1e53d4eb9f78ec16ee32
vhlongon <vhlongon@gmail.com>
2024-01-06 23:13:54 +0100

    refactor error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,15 +1,7 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { Card, CardContent, Container, Typography } from '@mui/material';
 import { useEffect } from 'react';
-import Button from '@mui/material/Button';
-import {
-  Card,
-  CardActions,
-  CardContent,
-  Container,
-  Typography,
-} from '@mui/material';
 
 type ErrorPageProps = {
   error: Error & {
@@ -18,7 +10,6 @@ type ErrorPageProps = {
 };
 
 export default function Error({ error }: ErrorPageProps) {
-  const router = useRouter();
   useEffect(() => {
     console.error(error);
   }, [error]);
@@ -27,27 +18,15 @@ export default function Error({ error }: ErrorPageProps) {
     <main className="flex min-h-screen flex-col justify-center items-center p-24 gap-8">
       <Card color="error">
         <CardContent>
-          <Typography variant="h5" component="h5">
+          <Typography variant="h5" component="h1">
             Error
           </Typography>
           <Typography variant="body1" component="p">
-            <Container maxWidth="sm" className="mt-2">
+            <Container maxWidth="sm" className="mt-2" component="span">
               <code className="">{JSON.stringify(error.message, null, 2)}</code>
             </Container>
           </Typography>
         </CardContent>
-        <CardActions className="justify-end">
-          <Button
-            variant="contained"
-            size="small"
-            color="error"
-            onClick={() => {
-              router.refresh();
-            }}
-          >
-            Try to reload
-          </Button>
-        </CardActions>
       </Card>
     </main>
   );
